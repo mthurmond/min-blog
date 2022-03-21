422be37c9acd0c7ccf53a24dc722b391960a5509
Matt Thurmond <matt.thurmond@gmail.com>
2022-03-21 17:16:06 -0400

    allow connection to heroku postgres instance

--- db/index.js
+++ db/index.js
@@ -6,11 +6,14 @@ const Sequelize = require('sequelize');
 //     storage: 'posts.db'
 // });
 
-// connect to pg
-const sequelize = new Sequelize(process.env.DATABASE_URL, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
-  });
+// connect to pg on local
+// const sequelize = new Sequelize(process.env.DATABASE_URL, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres'
+//   });
+
+// connect to heroku-postgres db --> all db parameters above baked into a single URI
+const sequelize = new Sequelize(process.env.DATABASE_URL); 
 
 const db = {
     sequelize, 
