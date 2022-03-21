71f636c02d5a1871fa62f12e1d20d1e68c3e3df7
Matt Thurmond <matt.thurmond@gmail.com>
2022-03-21 15:55:39 -0400

    replace sqlite with postgres

--- db/index.js
+++ db/index.js
@@ -1,9 +1,16 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: 'posts.db'
-});
+// connect to sqlite
+// const sequelize = new Sequelize({
+//     dialect: 'sqlite',
+//     storage: 'posts.db'
+// });
+
+// connect to pg
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  });
 
 const db = {
     sequelize, 
