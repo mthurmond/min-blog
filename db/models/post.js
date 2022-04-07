2330c4651c42a88971e143c9d1f2169c8d3118f1
Matt Thurmond <matt.thurmond@gmail.com>
2022-04-07 14:29:36 -0400

    add spaces when displaying post summary

--- db/models/post.js
+++ db/models/post.js
@@ -11,12 +11,12 @@ module.exports = (sequelize) => {
         }
         getShortBody() {
             let bodyHtml = this.body;
-            // remove image caption tags & text
-            let bodyNoCaptions = bodyHtml.replace(/<figcaption[^<]*<\/figcaption>/g, '');
-            // remove all other html tags, but not text inside
+            // convert closing tags (div, list item, double page breaks) and non-breaking space html entities to spaces
+            let bodyWithSpaces = bodyHtml.replace(/<\/div>|<\/li>|<br><br>|&nbsp;/g, ' ');
+            // remove image caption and pre-formatted (i.e. code) elements
+            let bodyNoCaptions = bodyWithSpaces.replace(/<(figcaption|pre)[^<]*<\/(figcaption|pre)>/g, '');
+            // remove all other html tags, but not the text between them
             let bodyText = bodyNoCaptions.replace(/<[^>]+>/g, '');
-            // remove non-breaking space html entities
-            bodyText = bodyText.replace(/&nbsp;/g, ' ');
             const shortBody = bodyText.length > 340 ? `${bodyText.slice(0,340)}...` : bodyText; 
             return shortBody; 
         }
