6b95da3eb95c1c3024eaedd4679eb9b8fbde4587
Matt Thurmond <matt.thurmond@gmail.com>
2022-05-16 17:13:03 -0400

    update app log msg

--- app.js
+++ app.js
@@ -76,5 +76,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(port, () => {
-    console.log(`App is now running on port ${port}.`); 
+    console.log(`Express blog is now running on port ${port}.`); 
 }); 
