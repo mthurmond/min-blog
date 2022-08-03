b26c643b848f513f818810169e5dd39cea9f68a4
Matt Thurmond <matt.thurmond@gmail.com>
2022-08-03 11:20:52 -0400

    warn user if exiting new or edit form with changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,61 +1,103 @@
-(function() {
-    var HOST = "https://d13txem1unpe48.cloudfront.net/"
-  
-    addEventListener("trix-attachment-add", function(event) {
-      if (event.attachment.file) {
-        uploadFileAttachment(event.attachment)
+(function () {
+  var HOST = "https://d13txem1unpe48.cloudfront.net/"
+
+  addEventListener("trix-attachment-add", function (event) {
+    if (event.attachment.file) {
+      uploadFileAttachment(event.attachment)
+    }
+  })
+
+  function uploadFileAttachment(attachment) {
+    uploadFile(attachment.file, setProgress, setAttributes)
+
+    function setProgress(progress) {
+      attachment.setUploadProgress(progress)
+    }
+
+    function setAttributes(attributes) {
+      attachment.setAttributes(attributes)
+    }
+  }
+
+  function uploadFile(file, progressCallback, successCallback) {
+    var key = createStorageKey(file)
+    var formData = createFormData(key, file)
+    var xhr = new XMLHttpRequest()
+
+    xhr.open("POST", HOST, true)
+
+    xhr.upload.addEventListener("progress", function (event) {
+      var progress = event.loaded / event.total * 100
+      progressCallback(progress)
+    })
+
+    xhr.addEventListener("load", function (event) {
+      if (xhr.status == 204) {
+        var attributes = {
+          url: HOST + key,
+          href: HOST + key + "?content-disposition=attachment"
+        }
+        successCallback(attributes)
       }
     })
-  
-    function uploadFileAttachment(attachment) {
-      uploadFile(attachment.file, setProgress, setAttributes)
-  
-      function setProgress(progress) {
-        attachment.setUploadProgress(progress)
+
+    xhr.send(formData)
+  }
+
+  function createStorageKey(file) {
+    var date = new Date()
+    var day = date.toISOString().slice(0, 10)
+    var name = date.getTime() + "-" + file.name
+    return ["tmp", day, name].join("/")
+  }
+
+  function createFormData(key, file) {
+    var data = new FormData()
+    data.append("key", key)
+    data.append("Content-Type", file.type)
+    data.append("file", file)
+    return data
+  }
+})();
+
+// alert user if they have unsaved changes and attempt to close page or navigate away
+// appies to /new and /edit pages
+document.addEventListener("DOMContentLoaded", function () {
+  let hasUnsavedChanges = false;
+  window.onbeforeunload = function () {
+    if (hasUnsavedChanges) {
+      // return string only shown in older browser versions, newer versions show browser default text
+      return "You have unsaved changes.";
+    }
+  };
+
+  const blogForm = document.querySelector("#blog-form");
+  if (blogForm) {
+    // fires when any text is typed, including in trix editor
+    this.body.addEventListener("keyup", () => {
+      hasUnsavedChanges = true;
+    })
+    // fires when inputs other than trix editor are changed, to ensure post status changes are recorded
+    blogForm.addEventListener("change", () => {
+      hasUnsavedChanges = true;
+    })
+
+    const blogFormButtons = document.querySelector("#blog-form-buttons");
+    blogFormButtons.addEventListener("click", (e) => {
+      switch (e.target.name) {
+        case 'submit-post':
+          window.onbeforeunload = ''
+          break;
+        case 'cancel-new-post':
+          window.onbeforeunload = ''
+          window.location.href = '/'
+          break;
+        case 'cancel-edited-post':
+          window.onbeforeunload = ''
+          // express passes post slug to pug, which assigns it to the 'pug' html attribute this js references
+          window.location.href = `/${e.target.dataset.slug}`
+          break;
       }
-  
-      function setAttributes(attributes) {
-        attachment.setAttributes(attributes)
-      }
-    }
-  
-    function uploadFile(file, progressCallback, successCallback) {
-      var key = createStorageKey(file)
-      var formData = createFormData(key, file)
-      var xhr = new XMLHttpRequest()
-  
-      xhr.open("POST", HOST, true)
-  
-      xhr.upload.addEventListener("progress", function(event) {
-        var progress = event.loaded / event.total * 100
-        progressCallback(progress)
-      })
-  
-      xhr.addEventListener("load", function(event) {
-        if (xhr.status == 204) {
-          var attributes = {
-            url: HOST + key,
-            href: HOST + key + "?content-disposition=attachment"
-          }
-          successCallback(attributes)
-        }
-      })
-  
-      xhr.send(formData)
-    }
-  
-    function createStorageKey(file) {
-      var date = new Date()
-      var day = date.toISOString().slice(0,10)
-      var name = date.getTime() + "-" + file.name
-      return [ "tmp", day, name ].join("/")
-    }
-  
-    function createFormData(key, file) {
-      var data = new FormData()
-      data.append("key", key)
-      data.append("Content-Type", file.type)
-      data.append("file", file)
-      return data
-    }
-  })();
+    })
+  }
+})
