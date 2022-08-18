48d6a8cc36ed5e3dfc0da98e8af5dabee904cef6
Matt Thurmond <matt.thurmond@gmail.com>
2022-08-18 15:44:40 -0400

    combine layout and layout-compact views

--- routes/index.js
+++ routes/index.js
@@ -140,7 +140,7 @@ router.get('/logout', function(req, res, next) {
 
 // GET /new
 router.get('/new', loginCheck, (req, res) => {
-    res.render('new', { title: "New post" }); 
+    res.render('new', { title: "New post", page: "new" }); 
 }); 
 
 // POST /new
@@ -160,7 +160,7 @@ router.post('/new', loginCheck, async (req, res, next) => {
 router.get('/edit/:slug', loginCheck, async (req, res, next) => {
     try {
         const post = await Post.findOne({where: {slug: req.params.slug}});
-        res.render('edit', { post, title: `Edit post | ${post.title}` });
+        res.render('edit', { post, title: `Edit post | ${post.title}`, page: 'edit' });
     } 
     catch (err) {
         err.message = 'This post could not be found.'
