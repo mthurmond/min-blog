d1b090fbb1f125a1403e54557b57d82cd95d42f7
Matt Thurmond <matt.thurmond@gmail.com>
2022-08-29 11:58:06 -0400

    cleanup app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,65 +1,3 @@
-(function () {
-  var HOST = "https://d13txem1unpe48.cloudfront.net/"
-
-  addEventListener("trix-attachment-add", function (event) {
-    if (event.attachment.file) {
-      uploadFileAttachment(event.attachment)
-    }
-  })
-
-  function uploadFileAttachment(attachment) {
-    uploadFile(attachment.file, setProgress, setAttributes)
-
-    function setProgress(progress) {
-      attachment.setUploadProgress(progress)
-    }
-
-    function setAttributes(attributes) {
-      attachment.setAttributes(attributes)
-    }
-  }
-
-  function uploadFile(file, progressCallback, successCallback) {
-    var key = createStorageKey(file)
-    var formData = createFormData(key, file)
-    var xhr = new XMLHttpRequest()
-
-    xhr.open("POST", HOST, true)
-
-    xhr.upload.addEventListener("progress", function (event) {
-      var progress = event.loaded / event.total * 100
-      progressCallback(progress)
-    })
-
-    xhr.addEventListener("load", function (event) {
-      if (xhr.status == 204) {
-        var attributes = {
-          url: HOST + key,
-          href: HOST + key + "?content-disposition=attachment"
-        }
-        successCallback(attributes)
-      }
-    })
-
-    xhr.send(formData)
-  }
-
-  function createStorageKey(file) {
-    var date = new Date()
-    var day = date.toISOString().slice(0, 10)
-    var name = date.getTime() + "-" + file.name
-    return ["tmp", day, name].join("/")
-  }
-
-  function createFormData(key, file) {
-    var data = new FormData()
-    data.append("key", key)
-    data.append("Content-Type", file.type)
-    data.append("file", file)
-    return data
-  }
-})();
-
 // alert user if they have unsaved changes and attempt to close page or navigate away
 // appies to /new and /edit pages
 document.addEventListener("DOMContentLoaded", function () {
@@ -149,7 +87,8 @@ document.addEventListener("DOMContentLoaded", function () {
       request.send(formData);
     }
   }
-    
+  
+  // NEW and EDIT pages --> warn user if they are leaving page with unsaved changes
   let hasUnsavedChanges = false;
   window.onbeforeunload = function () {
     if (hasUnsavedChanges) {
@@ -215,3 +154,66 @@ document.addEventListener("DOMContentLoaded", function () {
     })
   }
 })
+
+
+(function () {
+  var HOST = "https://d13txem1unpe48.cloudfront.net/"
+
+  addEventListener("trix-attachment-add", function (event) {
+    if (event.attachment.file) {
+      uploadFileAttachment(event.attachment)
+    }
+  })
+
+  function uploadFileAttachment(attachment) {
+    uploadFile(attachment.file, setProgress, setAttributes)
+
+    function setProgress(progress) {
+      attachment.setUploadProgress(progress)
+    }
+
+    function setAttributes(attributes) {
+      attachment.setAttributes(attributes)
+    }
+  }
+
+  function uploadFile(file, progressCallback, successCallback) {
+    var key = createStorageKey(file)
+    var formData = createFormData(key, file)
+    var xhr = new XMLHttpRequest()
+
+    xhr.open("POST", HOST, true)
+
+    xhr.upload.addEventListener("progress", function (event) {
+      var progress = event.loaded / event.total * 100
+      progressCallback(progress)
+    })
+
+    xhr.addEventListener("load", function (event) {
+      if (xhr.status == 204) {
+        var attributes = {
+          url: HOST + key,
+          href: HOST + key + "?content-disposition=attachment"
+        }
+        successCallback(attributes)
+      }
+    })
+
+    xhr.send(formData)
+  }
+
+  function createStorageKey(file) {
+    var date = new Date()
+    var day = date.toISOString().slice(0, 10)
+    var name = date.getTime() + "-" + file.name
+    return ["tmp", day, name].join("/")
+  }
+
+  function createFormData(key, file) {
+    var data = new FormData()
+    data.append("key", key)
+    data.append("Content-Type", file.type)
+    data.append("file", file)
+    return data
+  }
+})();
